42112846576c0013e2f6db08feb41d3c1f58c197
p58102 <p58102@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-02-03 15:28:25 +0000

    Structure du projet

--- TDBSolver/public_html/js/printMap.js
+++ TDBSolver/public_html/js/printMap.js
@@ -1,10 +1,12 @@
-function initialize() {
+define("printMap", 
+["jQuery","googleApi"],function(){
+/*function initialize() {
     var mapOptions = {
         zoom: 16,
         center: new google.maps.LatLng(50.634327, 3.048550)
     };
     
-    var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+    map = new google.maps.Map(document.getElementById("map"),mapOptions);
     
     var isen = new google.maps.Marker({
         position: new google.maps.LatLng(50.634327, 3.048550),
@@ -14,6 +16,8 @@ function initialize() {
 
 };
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);*/
+$("body").html("BONJOUR");
+});
 
 
