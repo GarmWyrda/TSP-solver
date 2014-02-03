837a60a9996a66db158b4455098aaa7a9c3274ce
p58114 <p58114@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-02-03 14:11:51 +0000

    ajout du printMap

--- TDBSolver/public_html/js/boot.js
+++ TDBSolver/public_html/js/boot.js
@@ -2,7 +2,9 @@
 require.config({
     paths: {
         jquery: 'http://code.jquery.com/jquery-1.10.1.min.js',
-        bootstrap : 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js'
+        bootstrap : 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js',
+        googleApi : 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false',
+        printMap : 'printmap.js'
     }
 });
  
---
+++ TDBSolver/public_html/js/printMap.js
@@ -0,0 +1,19 @@
+function initialize() {
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(50.634327, 3.048550)
+    };
+    
+    var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+    
+    var isen = new google.maps.Marker({
+        position: new google.maps.LatLng(50.634327, 3.048550),
+        map: map,
+        title: 'ISEN'
+    });
+
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
