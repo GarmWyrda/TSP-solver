d687dab8bba6db4c2cd6ec9a34a8cc9c3f3a47de
p58102 <p58102@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-02-03 14:28:44 +0000

    Structure du projet

--- TDBSolver/public_html/js/boot.js
+++ TDBSolver/public_html/js/boot.js
@@ -1,21 +1,24 @@
 // création des alias/paths
 require.config({
     paths: {
-        jquery: 'http://code.jquery.com/jquery-1.10.1.min.js',
-        bootstrap : 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js',
+        jQuery: 'http://code.jquery.com/jquery-1.10.1.min',
+        bootstrap : 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min',
         googleApi : 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false',
-        printMap : 'printmap.js'
+        printMap : 'printMap'
+    },
+    shim: {
+        'jQuery': {
+            exports: 'jQuery'
+        }                       
     }
 });
- 
-// module commun à tout le site
-define('common', [
-    'jquery'
-], function($) {
-    console.log('boot loaded');
-});
- 
-// on charge le module commun
-require(['common']);
 
+require(["jQuery", "bootstrap","googleApi","printMap"], init);
+
+function init() {
+	
+	console.log("Loading sucessful");
+
+}
+ 
 
