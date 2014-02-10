97180483019e91f5bc65fffd53147a78faf2161f
p58114 <p58114@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-02-10 15:05:56 +0000

    script chargement des points sur la map avec numero

---
+++ TDBSolver/public_html/js/loadplace.js
@@ -0,0 +1,37 @@
+var places = {
+    list:[],
+    marker:[]
+};
+
+var loadfile = function () {
+    $.getJSON('ressources/emplacements.json', function(data) {
+        places.list = data;
+    });
+};
+
+loadfile();
+
+var loadEmplacement = function(nb) {
+    var id=0;
+    var Lat=0;
+    var Lng=0;
+    
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(50.634327, 3.048550)
+    };
+    
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+    for(var i=0;i<nb;i++){
+        id = places.list[i].id;
+        Lat = places.list[i].lat;
+        Lng = places.list[i].lng;
+        places.marker[i] = new google.maps.Marker({
+            position: new google.maps.LatLng(Lat, Lng),
+            map: map,
+            title: id,
+            icon:'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+id+'|FF776B|000000'
+        }); 
+    } 
+};
