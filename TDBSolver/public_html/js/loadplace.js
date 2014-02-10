f42ae3d5aaace0efb83e20b154c182e5be89fab8
p58102 <p58102@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-02-10 15:23:30 +0000

    Require maps

--- TDBSolver/public_html/js/loadplace.js
+++ TDBSolver/public_html/js/loadplace.js
@@ -1,37 +1,45 @@
-var places = {
-    list:[],
-    marker:[]
-};
+define('loadplace', ['jQuery'],function(){
+    var places = {
+        list:[],
+        marker:[]
+    };
 
-var loadfile = function () {
-    $.getJSON('ressources/emplacements.json', function(data) {
-        places.list = data;
-    });
-};
+    var loadfile = function () {
+        $.getJSON('ressources/emplacements.txt', function(data) {
+            places.list = data;
+        });
+    };
 
-loadfile();
+    loadfile();
 
-var loadEmplacement = function(nb) {
-    var id=0;
-    var Lat=0;
-    var Lng=0;
-    
-    var mapOptions = {
-        zoom: 14,
-        center: new google.maps.LatLng(50.634327, 3.048550)
+    var loadEmplacement = function(nb) {
+        var id=0;
+        var Lat=0;
+        var Lng=0;
+
+        var mapOptions = {
+            zoom: 14,
+            center: new google.maps.LatLng(50.634327, 3.048550)
+        };
+
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        for(var i=0;i<nb;i++){
+            id = places.list[i].id;
+            Lat = places.list[i].lat;
+            Lng = places.list[i].lng;
+            places.marker[i] = new google.maps.Marker({
+                position: new google.maps.LatLng(Lat, Lng),
+                map: map,
+                title: id,
+                icon:'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+id+'|FF776B|000000'
+            }); 
+        } 
     };
     
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    
-    for(var i=0;i<nb;i++){
-        id = places.list[i].id;
-        Lat = places.list[i].lat;
-        Lng = places.list[i].lng;
-        places.marker[i] = new google.maps.Marker({
-            position: new google.maps.LatLng(Lat, Lng),
-            map: map,
-            title: id,
-            icon:'https://chart.googleapis.com/chart?chst=d_map_pin_letter&chld='+id+'|FF776B|000000'
-        }); 
-    } 
-};
+    $("#list").on("change",function(){
+        console.log($("#list").val());
+        loadEmplacement($("#list").val());
+        $("#logger").append('<p class="alert-success">Map changed</p>');
+    });
+});
