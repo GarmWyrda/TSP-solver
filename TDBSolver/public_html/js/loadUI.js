620d4fb6023ca40f61baab46feb9f24174a063f9
p58102 <p58102@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-02-10 14:42:47 +0000

    Require maps

--- TDBSolver/public_html/js/loadUI.js
+++ TDBSolver/public_html/js/loadUI.js
@@ -3,15 +3,13 @@
  * and open the template in the editor.
  */
 define('loadUI',['jQuery'],function(){
-    console.log(string);
-    $.getJSON("../ressources/emplacement.txt",function(data){
+    $.getJSON("ressources/emplacements.txt",function(data){
        var length = data.length;
        var string = "";
        for(var i = 1;i<length+1;i++){
            string += "<option value=" + i + ">" + i + "</option> ";
        }
        $("#list").html(string);
-       console.log(string);
     });
 });
 
