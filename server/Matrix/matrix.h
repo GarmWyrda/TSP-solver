ff1b5fc39edc47ba584965bc5d775fc425a57ee4
p58102 <p58102@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-03-10 13:14:26 +0000

    Structure projet

--- server/Matrix/matrix.h
+++ server/Matrix/matrix.h
@@ -7,7 +7,39 @@
 
 #ifndef MATRIX_H
 #define	MATRIX_H
+#include <vector>
+#include "IndexOutOfBoundsException.h" // fichier à créer et chemin à changer selon l'organisation de votre projet
+using std::vector;
 
+template<class T> class Matrix{
+    private:
+        int rows;
+        int cols;
+        int emptyVal;
+        vector<vector<T>> data;
+
+    public :
+
+    // -------------- interdit de modifier cette partie -------------- //
+    Matrix(int nbRows, int nbColumns, T emptyValue);
+
+    T getEmptyValue();
+
+    int getNbRows();
+    void addRow(int rowIndex) throw(IndexOutOfBoundsException);
+    void removeRow(int rowIndex) throw(IndexOutOfBoundsException);
+
+    int getNbColumns();
+    void addColumn(int colIndex) throw(IndexOutOfBoundsException);
+    void removeColumn(int colIndex) throw(IndexOutOfBoundsException);
+
+    T getValue(int rowIndex, int colIndex) throw(IndexOutOfBoundsException);
+    void setValue(int rowIndex, int colIndex, T value) throw(IndexOutOfBoundsException);
+
+    ~Matrix();
+    // --------------------------------------------------------------- //
+
+};
 
 
 #endif	/* MATRIX_H */
