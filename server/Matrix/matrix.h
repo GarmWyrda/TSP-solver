a66fcdd09714b866c4683c2d3bf9db378ac1cc8c
p58102 <p58102@5e7319d9-d508-4ec5-a06e-588cd56c6193>
2014-03-10 15:06:05 +0000

    Constructeur implémenté

--- server/Matrix/matrix.h
+++ server/Matrix/matrix.h
@@ -11,68 +11,74 @@
 #include "IndexOutOfBoundsException.h" // fichier à créer et chemin à changer selon l'organisation de votre projet
 using std::vector;
 
-template<class T> class Matrix{
-    private:
-        int rows;
-        int cols;
-        int emptyVal;
-        vector< vector<T*> > data;
+template<class T> class Matrix {
+private:
+    int rows;
+    int cols;
+    int emptyVal;
+    vector< vector<T*> > data;
 
-    public :
+public:
 
     // -------------- interdit de modifier cette partie -------------- //
-    Matrix(int nbRows, int nbColumns, T emptyValue);
+    Matrix(int nbRows, int nbColumns, T emptyValue); //done
 
-    T getEmptyValue();//done
+    T getEmptyValue(); //done
 
-    int getNbRows();// done
-    void addRow(int rowIndex) throw(IndexOutOfBoundsException);
-    void removeRow(int rowIndex) throw(IndexOutOfBoundsException);
+    int getNbRows(); // done
+    void addRow(int rowIndex) throw (IndexOutOfBoundsException);
+    void removeRow(int rowIndex) throw (IndexOutOfBoundsException);
 
-    int getNbColumns();// done
-    void addColumn(int colIndex) throw(IndexOutOfBoundsException);
-    void removeColumn(int colIndex) throw(IndexOutOfBoundsException);
+    int getNbColumns(); // done
+    void addColumn(int colIndex) throw (IndexOutOfBoundsException);
+    void removeColumn(int colIndex) throw (IndexOutOfBoundsException);
 
-    T getValue(int rowIndex, int colIndex) throw(IndexOutOfBoundsException);
-    void setValue(int rowIndex, int colIndex, T value) throw(IndexOutOfBoundsException);
+    T getValue(int rowIndex, int colIndex) throw (IndexOutOfBoundsException);
+    void setValue(int rowIndex, int colIndex, T value) throw (IndexOutOfBoundsException);
 
-    ~Matrix();
+    ~Matrix(); //done
     // --------------------------------------------------------------- //
 
 };
 
-template<class T> Matrix<T>::Matrix(int nbRows,int nbColumns,T emptyValue){
-    if(nbRows < 0){
+template<class T> Matrix<T>::Matrix(int nbRows, int nbColumns, T emptyValue) {
+    if (nbRows < 0) {
         nbRows = 0;
     }
-    if(nbColumns){
+    if (nbColumns < 0) {
         nbColumns = 0;
     }
     this->rows = nbRows;
     this->cols = nbColumns;
     this->emptyVal = emptyValue;
     this->data = vector< vector<T*> >(nbRows);
-    for(int&i:this->data){
-        this->data[i] = vector<T*>(nbColumns,NULL);
+    for(int i;i<this->data.size();i++) {
+        this->data[i] = vector<T*>(nbColumns, NULL);
     }
 };
 
-template<class T> int Matrix<T>::getNbRows(){
+template<class T> Matrix<T>::~Matrix() {
+
+};
+
+template<class T> int Matrix<T>::getNbRows() {
     return this->rows;
 };
-template<class T> int Matrix<T>::getNbColumns(){
+
+template<class T> int Matrix<T>::getNbColumns() {
     return this->cols;
 };
-template<class T> T Matrix<T>::getEmptyValue(){
+
+template<class T> T Matrix<T>::getEmptyValue() {
     return this->emptyVal;
 };
 
-template<class T> void Matrix<T>::addRow(int rowIndex) throw(IndexOutOfBoundsException){
-    if (rowIndex<0 ){
-        IndexOutOfBoundsException(rowIndex,0,false);
+template<class T> void Matrix<T>::addRow(int rowIndex) throw (IndexOutOfBoundsException) {
+    if (rowIndex < 0) {
+        IndexOutOfBoundsException(rowIndex, 0, false);
     }
     if (rowIndex>this->rows) {
-        IndexOutOfBoundsException(rowIndex,this->rows,false);
+        IndexOutOfBoundsException(rowIndex, this->rows, false);
     }
     vector<vector<T*> > row;
     
--- server/Matrix/test.cpp
+++ server/Matrix/test.cpp
@@ -1,5 +1,5 @@
 #include "matrix.h"
 
 int main(){
-    
+    Matrix<int> testMatrix = Matrix<int>(0,0,0);
 }
